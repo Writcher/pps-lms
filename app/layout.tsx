b5f4975f840c815af941295755d80e7b0d90fae8
Joaquín Giménez <joaquingim77@gmail.com>
2024-11-10 22:32:59 -0300

    feat: tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "LMS",
   description: "Proyecto PPS Gimenez",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
